de692dad6717ea96e36e6108679712f55eba83b8
Tracy Ogutu <tracyogutu@gmail.com>
2020-07-04 21:22:00 +0300

    remove automatic profile creation

--- app/User.php
+++ app/User.php
@@ -37,6 +37,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // public static function boot(){
+    //     static::created(function($user){
+    //         $user->profile()->create([
+    //             'title'=>$user->username,
+    //         ]);
+    //     });
+    // }
     public function profile(){
         return $this->hasOne(Profile::class);
     }
