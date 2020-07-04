db7483441937853392bac13585fb8a373a6980b6
Tracy Ogutu <tracyogutu@gmail.com>
2020-07-04 21:21:38 +0300

    Disable mass assignment

--- app/Profile.php
+++ app/Profile.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    //disables mass assignment
+    protected $guarded =[];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
