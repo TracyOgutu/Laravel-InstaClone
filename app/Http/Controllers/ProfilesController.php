107a08abe0606b3634b4779e7e79515b3f5301ae
Tracy Ogutu <tracyogutu@gmail.com>
2020-07-08 16:43:22 +0300

    check if the authenticated user follows a profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -11,9 +11,14 @@ class ProfilesController extends Controller
 {
     //
     public function index(User $user)
-    {
+
+    { 
+       // Checking if the authenticated user follows the profile
+        $follows=(auth()->user())? auth()->user()->following->contains($user->id):false;
+
+       // dd($follows);
        
-        return view('profiles.index',compact('user'));
+        return view('profiles.index',compact('user','follows'));
     }
     public function edit(User $user){
 
